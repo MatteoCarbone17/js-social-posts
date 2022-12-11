bf2ed4eb577b01fac51ace3d97a20320072f3c3b
MatteoCarbone17 <matteocarbone17@live.com>
2022-12-11 19:08:42 +0100

    add Milestone 2 && bonus

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+/**
+ * Descrizione
+Ricreiamo un feed social aggiungendo al layout di base fornito, il nostro script JS in cui:
+Milestone 1 -
+Prendendo come riferimento il layout di esempio presente nell'html, stampiamo i post nel nostro feed, (rimuovendo il post di prova dall'html).
+Milestone 2 -
+Se clicchiamo sul tasto "Mi Piace" cambiamo il colore al testo del bottone e incrementiamo il counter dei likes relativo.
+Salviamo in un secondo array gli id dei post ai quali abbiamo messo il like.
+ */
+
 const posts = [
     {
         "id": 1,
@@ -56,22 +66,9 @@ const posts = [
     }
 ];
 
-
-/**
- * Descrizione
-Ricreiamo un feed social aggiungendo al layout di base fornito, il nostro script JS in cui:
-Milestone 1 -
-Prendendo come riferimento il layout di esempio presente nell'html, stampiamo i post nel nostro feed, (rimuovendo il post di prova dall'html).
-Milestone 2 -
-Se clicchiamo sul tasto "Mi Piace" cambiamo il colore al testo del bottone e incrementiamo il counter dei likes relativo.
-Salviamo in un secondo array gli id dei post ai quali abbiamo messo il like.
- */
-
-
-const postWrapper = document.getElementById('container');
+const likedPostsArray = [];
 
 function addStructure(postData) {
-
     return ` <div class="post">
     <div class="post__header">
         <div class="post-meta">                    
@@ -80,7 +77,7 @@ function addStructure(postData) {
             </div>
             <div class="post-meta__data">
                 <div class="post-meta__author">${postData.author.name}</div>
-                <div class="post-meta__time">${postData.created}</div>
+                <div class="post-meta__time">${new Date(postData.created).toLocaleDateString('it-It')}</div>
             </div>                    
         </div>
     </div>
@@ -91,37 +88,54 @@ function addStructure(postData) {
     <div class="post__footer">
         <div class="likes js-likes">
             <div class="likes__cta">
-                <a class="like-button  js-like-button" href="#" data-postid="1">
+                <a class="like-button  js-like-button" data-postid="${postData.id}">
                     <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
                     <span class="like-button__label">Mi Piace</span>
                 </a>
             </div>
             <div class="likes__counter">
-                Piace a <b id="like-counter-1" class="js-likes-counter">${postData.likes}</b> persone
+                Piace a <b id="like-counter-${postData.id}" class="js-likes-counter">${postData.likes}</b> persone
             </div>
         </div> 
     </div>            
 </div> `
-
 }
 
-posts.forEach((element) => {
 
-    postWrapper.innerHTML += addStructure(element);
 
+function listenToClick(collection) {
+    for (let button of collection) {
+        button.addEventListener('click', () => {
+            const postId = button.dataset.postid;
+            const likeCounter = document.getElementById(`like-counter-${postId}`);
+            const likedPostIndex = likedPostsArray.findIndex(id => id === postId);
+
+            if (!likedPostsArray.includes(postId)) {
+                button.classList.add('like-button--liked')
+                likeCounter.innerHTML++;
+                likedPostsArray.push(postId);
+            } else {
+                likeCounter.innerHTML--;
+                likedPostsArray.splice(likedPostIndex, 1)
+                button.classList.remove('like-button--liked')
+            };
+        });
+    };
+};
+
+const container = document.getElementById('container');
+
+posts.forEach((post) => {
+    container.innerHTML += addStructure(post);
 });
 
+const collection = document.getElementsByClassName('like-button');
+
+listenToClick(collection);
 
 
-for (let i = 0; i < posts.length; i++) {
-   
-    let counter = posts[i].likes
-    console.log(counter)
-
-    const postWrapper = document.getElementById('');
 
 
-}
 
 
 
